9d5335f9ccdad62105f0147705dae9324c7f444a
Rehan Adi Satrya <47605698+rehanadi30@users.noreply.github.com>
2021-01-28 20:55:28 +0700

    Tutorial Done
    Skor belum bertambah, Papan belum bisa ke ujung atas/bawah

---
+++ src/Pong/Assets/BallControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if(rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        } 
+        else 
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // ResetBall dipanggil setiap game dimulai
+    void ResetBall()
+    {
+        rb2d.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    // Restart dipanggil setiap restart dipencet
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+    // OnCollision dipanggil setiap benturan terjadi
+    void OnCollisionEnter2D (Collision2D coll) 
+    {
+        if(coll.collider.CompareTag("Player"))
+        {
+            Vector2 vel;
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+            rb2d.velocity = vel;
+        }
+    }
+}
